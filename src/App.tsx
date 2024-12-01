d14458622a950049b7743e920cbd687f53e26be1
DvTox <49532943+DvTox@users.noreply.github.com>
2024-12-01 21:52:22 +0100

    Initial Commit 🚀

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-slate-500 w-screen h-screen'></div>
+    </>
+  )
+}
+
+export default App
